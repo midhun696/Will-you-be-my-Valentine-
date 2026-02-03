472d3be7b170fa51c87bb2b678d3b0eec84d12de
midhun696 <akshayakshaypes123@gmail.com>
2026-02-03 14:33:32 -0500

    CHANGED

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
+    "sherikum",
+    "pinne ??",
+    "kALLAM PODY ?",
     "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "ONNUDE ALOCHICHITTU PORE!",
+    "ENIKU VESHAMAM AKKUTTO",
+    "ORAPICHO",
+    "ANNA NGAN POVA",
+    "NGAN NIRTHY INNI CHOIKANNILA",
+    "PODY NGAN POVULLA YES PARANGO1 ❤️",
 ];
 
 let messageIndex = 0;
